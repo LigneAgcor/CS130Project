b5fde678437fbcf06315e3883cbf9b54a95bb992
LigneAgcor <nigel.cargo@yahoo.com>
2018-03-22 19:45:19 +0800

    Added FileWriter

--- CS130_Midterms/src/Reader.java
+++ CS130_Midterms/src/Reader.java
@@ -4,28 +4,38 @@ public class Reader{
 	public Reader() throws Exception
 	{
 		FileReader file = null;
+		FileWriter output = null;
 		try {
 		file = new FileReader("src\\text files\\text.txt");//"C:\\Users\\great_000\\Documents\\School\\4th year\\2nd Sem\\CS130\\Midterm Proj\\CS130Project\\CS130_Midterms\\src\\text.txt");
+		output = new FileWriter("src\\text files\\output.txt");
 		}
 		catch(FileNotFoundException ae){
 			System.out.println("File not Found");
 		}
 		BufferedReader br = new BufferedReader(file);
+		BufferedWriter bw = new BufferedWriter(output);
+		
 		
 		String currentLine;
+		String allRead = "";
+		boolean startnewLex = true;
 		
 		while ((currentLine = br.readLine()) != null)
 		{
 			System.out.println(currentLine);
+			allRead += currentLine;
+			allRead += " ";
 		}
 		
+		System.out.println(allRead);
+		
 	}
+	
 	
 	public static void main(String args[]) throws Exception
 	{
 		Reader test = new Reader();
 		
-		System.out.println("Run testing");
 	}
 	
 	
